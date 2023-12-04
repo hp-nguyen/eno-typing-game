81e4df92ea01763ba28f93c5b526a7f8bb940d50
HP Nguyen <phucvovinam97@gmail.com>
2023-12-04 23:48:38 +0700

    Done game

--- assets/Scripts/GameManager.js
+++ assets/Scripts/GameManager.js
@@ -3,13 +3,22 @@ cc.Class({
   extends: cc.Component,
 
   properties: {
+    totalTime: {
+      default: 60,
+      serializable: true,
+    },
     playerInput: cc.EditBox,
     paragraphLayout: cc.Layout,
     wordPrefab: cc.Prefab,
+    resultTable: cc.Layout,
+    wpmLabel: cc.Label,
+    correctWordsLabel: cc.Label,
+    wrongWordsLabel: cc.Label,
   },
 
   onLoad() {
-    this.words = randomWords(25);
+    this.isPlaying = true;
+    this.words = randomWords(24);
     this.paragraph = this.words.join(' ');
     this.wordLabels = [];
     for (let word of this.words) {
@@ -17,7 +26,9 @@ cc.Class({
     }
     this.curWordIndex = 0;
     this.curWord = this.words[this.curWordIndex];
-    this.trueWords = 0;
+    this.correctWords = 0;
+    this.wrongWords = 0;
+    this.timer = 0;
   },
 
   start() {},
@@ -28,15 +39,19 @@ cc.Class({
   },
   checkCurTyping() {
     if (this.playerInput.string.trim() === this.curWord) {
-      this.trueWords += 1;
+      this.correctWords += 1;
       this.wordLabels[this.curWordIndex].getComponent(cc.Label).node.color = cc.Color.GREEN;
     } else {
       this.wordLabels[this.curWordIndex].getComponent(cc.Label).node.color = cc.Color.RED;
+      this.wrongWords += 1;
     }
     this.playerInput.string = '';
     this.playerInput.blur();
     this.playerInput.focus();
     this.curWordIndex += 1;
+    if (this.checkEndGame()) {
+      this.onEndGame();
+    }
     this.curWord = this.words[this.curWordIndex];
   },
   generateNewLabel(text) {
@@ -46,8 +61,24 @@ cc.Class({
     this.wordLabels.push(newLabel);
   },
   checkEndGame() {
-    if (this.timer <= 0 || this.curWordIndex === this.words.length) {
-    }
+    if (this.timer >= this.totalTime || this.curWordIndex === this.words.length) return true;
+    return false;
   },
-  // update (dt) {},
+  onEndGame() {
+    this.playerInput.node.active = false;
+    this.paragraphLayout.node.active = false;
+    this.wpmLabel.string = `WPM: ${Math.trunc((this.correctWords / this.timer) * 60)}`;
+    this.correctWordsLabel.string = `Correct words: ${this.correctWords}`;
+    this.wrongWordsLabel.string = `Wrong words: ${this.wrongWords}`;
+    this.resultTable.node.active = true;
+    this.isPlaying = false;
+  },
+  update(dt) {
+    if (this.isPlaying) {
+      if (this.checkEndGame()) {
+        this.onEndGame();
+      }
+    }
+    if (this.timer <= this.totalTime) this.timer += dt;
+  },
 });
